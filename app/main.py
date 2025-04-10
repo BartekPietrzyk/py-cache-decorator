d25fd5ba131a6d6a3f96adb35dbd8e0cb5508bed
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-04-10 12:58:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cache_date = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
+    def inner(*args) -> Callable:
+        cache_date = {}
         if args in cache_date:
             print("Getting from cache")
         else:
             print("Calculating new result")
             cache_date[args] = func(*args)
         return cache_date[args]
-    return wrapper
+    return inner
